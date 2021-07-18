a87341596d865088c6e37e5036f9cbb513dfb0e7
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2021-07-18 15:57:16 +1000

    Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,33 @@
 module.exports = {
-  extends: ['@terenceodonoghue/base'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/ban-ts-comment': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
     'no-console': 'off',
     'no-underscore-dangle': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
   },
 };
